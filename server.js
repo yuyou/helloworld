367ecc366d81660d82aeb466039c0b8b52fe0a03
vagrant <vagrant@node01>
2016-03-23 13:12:47 +0000

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World 2!");
+  response.end("Hello World 3!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8081,"0.0.0.0");
